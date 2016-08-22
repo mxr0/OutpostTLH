23f12e61471349edff91eb6c4149d7b7f701b440
Maximilian Ronniger <maximilian.ronniger@rise-word.com>
2016-08-22 07:03:49 +0200

    initial add of the English language set.

---
+++ script/DataText_en.js
@@ -0,0 +1,220 @@
+// --- Base
+TextRepository.add(TerrainLayer.Surface, "Surface");
+TextRepository.add(TerrainLayer.Underground, "Level1");
+TextRepository.add(TerrainLayer.Deep, "Level2");
+
+TextRepository.add("Base", "Base");
+TextRepository.add("Advanced", "Advanced");
+TextRepository.add("Extended", "Extended");
+
+TextRepository.add("RoboDozer", "RoboDozer");
+TextRepository.add("RoboDigger", "RoboDigger");
+TextRepository.add("RoboMiner", "RoboMiner);
+
+// --- Testi
+TextRepository.add("Active", "Active");
+TextRepository.add("Idle", "Idle");
+TextRepository.add("Production", "Production");
+TextRepository.add("Consumption", "Consumption");
+TextRepository.add("Total", "Total");
+TextRepository.add("Progress", "Progress");
+TextRepository.add("Stored", "Stored");
+TextRepository.add("Surplus", "Surplus");
+TextRepository.add("Materials", "Materials");
+
+TextRepository.add("unavailable", "unavailable");
+TextRepository.add("Available", "Available");
+TextRepository.add("ProductionTitle", "ProductionTitle");
+TextRepository.add("Working", "Working");
+TextRepository.add("ResearchTitle", "ResearchTitle");
+TextRepository.add("Researching", "Researching");
+TextRepository.add("ReportTitle", "ReportTitle");
+TextRepository.add("ReportBase", "ReportBase");
+TextRepository.add("ReportEvents", "ReportEvents");
+TextRepository.add("HelpTitle", "HelpTitle");
+TextRepository.add("HelpBase", "HelpBase");
+TextRepository.add("Buildings", "Buildings");
+TextRepository.add("HelpTutorial_Base", "HelpTutorial_Base");
+
+TextRepository.add("TerrainLayer", "TerrainLayer");
+
+TextRepository.add("BuildingTitle", "BuildingTitle");
+TextRepository.add("BuildingTime", "BuildingTime");
+TextRepository.add("BuildingCost", "BuildingCost");
+TextRepository.add("BuildingCapacity", "BuildingCapacity");
+
+TextRepository.add("ProductionTitle", "ProductionTitle");
+TextRepository.add("BuildingConsumption", "BuildingConsumption");
+TextRepository.add("BuildingProduction", "BuildingProduction");
+TextRepository.add("BuildingWaste", "BuildingWaste");
+TextRepository.add("Requirements", "Requirements");
+
+TextRepository.add("Humans", "Humans");
+TextRepository.add("Population", "Population");
+TextRepository.add("wellness", "wellness");
+TextRepository.add("happiness", "happiness");
+TextRepository.add("infants", "infants");
+TextRepository.add("students", "students");
+TextRepository.add("researchers", "researchers");
+TextRepository.add("scientists", "scientists");
+TextRepository.add("retirees", "retirees");
+TextRepository.add("deads", "deads");
+
+// --- Materiali
+TextRepository.add("researchElaborationUnit", "researchElaborationUnit");
+TextRepository.add("researchStructureSurface", "researchStructureSurface");
+TextRepository.add("researchStructureSurfaceAdv", "researchStructureSurfaceAdv");
+TextRepository.add("researchStructureUnderground", "researchStructureUnderground");
+TextRepository.add("researchStructureUndergroundAdv", "researchStructureUndergroundAdv");
+TextRepository.add("researchStructureDeep", "researchStructureDeep");
+TextRepository.add("researchStructureDeepAdv", "researchStructureDeepAdv");
+TextRepository.add("productionStandardStructure", "productionStandardStructure");
+TextRepository.add("researchUnit", "researchUnit");
+TextRepository.add("productionUnit", "productionUnit");
+TextRepository.add("emergencyTeamUnit", "emergencyTeamUnit");
+TextRepository.add("controlUnit", "controlUnit");
+TextRepository.add("habitatUnit", "habitatUnit");
+TextRepository.add("repairUnit", "repairUnit");
+TextRepository.add("foodUnit", "foodUnit");
+TextRepository.add("residentialUnit", "residentialUnit");
+TextRepository.add("educationUnit", "educationUnit");
+TextRepository.add("highEducationUnit", "highEducationUnit");
+TextRepository.add("recreationalUnit", "recreationalUnit");
+TextRepository.add("pharmaceuticalUnit", "pharmaceuticalUnit");
+TextRepository.add("medicalUnit", "medicalUnit");
+TextRepository.add("nurseryUnit", "nurseryUnit");
+TextRepository.add("goodsUnit", "goodsUnit");
+
+TextRepository.add("power", "power");
+TextRepository.add("mineralOre", "mineralOre");
+TextRepository.add("cristalOre", "cristalOre");
+TextRepository.add("metalOre", "metalOre");
+TextRepository.add("radioactiveOre", "radioactiveOre");
+TextRepository.add("ironBar", "ironBar");
+TextRepository.add("fusionWaste", "fusionWaste");
+TextRepository.add("radioactiveWaste", "radioactiveWaste");
+TextRepository.add("toxicWaste", "toxicWaste");
+
+TextRepository.add("roboticStorage", "roboticStorage");
+TextRepository.add("genericStorage", "genericStorage");
+TextRepository.add("radioactiveStorage", "radioactiveStorage");
+
+
+// --- Edifici
+TextRepository.add("Pipes", "Pipes");
+
+TextRepository.add("Pipe_nesw_surface", "Pipe_nesw_surface");
+TextRepository.add("Pipe_ns_surface", "Pipe_ns_surface");
+TextRepository.add("Pipe_ew_surface", "Pipe_ew_surface");
+TextRepository.add("Pipe_d_surface", "Pipe_d_surface");
+TextRepository.add("Pipe_nesw_underground", "Pipe_nesw_underground");
+TextRepository.add("Pipe_ns_underground", "Pipe_ns_underground");
+TextRepository.add("Pipe_ew_underground", "Pipe_ew_underground");
+TextRepository.add("Pipe_u_underground", "Pipe_u_underground");
+TextRepository.add("Pipe_d_underground", "Pipe_d_underground");
+TextRepository.add("Pipe_nesw_deep", "Pipe_nesw_deep");
+TextRepository.add("Pipe_ns_deep", "Pipe_ns_deep");
+TextRepository.add("Pipe_ew_deep", "Pipe_ew_deep");
+TextRepository.add("Pipe_u_deep", "Pipe_u_deep");
+
+TextRepository.add("LandingModule", "LandingModule");
+TextRepository.add("SupportModule", "SupportModule");
+TextRepository.add("MicrowavePlant", "MicrowavePlant");
+TextRepository.add("MicrowaveSatellite", "MicrowaveSatellite");
+
+TextRepository.add("ElectrostaticCoil", "ElectrostaticCoil");
+TextRepository.add("Geothermal", "Geothermal");
+TextRepository.add("StorageArea", "StorageArea");
+TextRepository.add("Tokamak", "Tokamak");
+TextRepository.add("TokamakAdv", "TokamakAdv");
+TextRepository.add("Capacitor", "Capacitor");
+TextRepository.add("CommandCenter", "CommandCenter");
+TextRepository.add("MaintenanceCenter", "MaintenanceCenter");
+TextRepository.add("RoboRemittance", "RoboRemittance");
+TextRepository.add("RoboRemittanceAdv", "RoboRemittanceAdv");
+TextRepository.add("RoboCommander", "RoboCommander");
+TextRepository.add("RoboCommanderAdv", "RoboCommanderAdv");
+TextRepository.add("EnvironmentalControl", "EnvironmentalControl");
+TextRepository.add("EnvironmentalControlAdv", "EnvironmentalControlAdv");
+TextRepository.add("Agridome", "Agridome");
+TextRepository.add("AgridomeAdv", "AgridomeAdv");
+TextRepository.add("AgridomeIntensive", "AgridomeIntensive");
+TextRepository.add("VerticalFarm", "VerticalFarm");
+TextRepository.add("VerticalFarmAdv", "VerticalFarmAdv");
+TextRepository.add("Residential", "Residential");
+TextRepository.add("ResidentialAdv", "ResidentialAdv");
+TextRepository.add("Nursery", "Nursery");
+TextRepository.add("MedicalStructure", "MedicalStructure");
+TextRepository.add("MedicalStructureAdv", "MedicalStructureAdv");
+TextRepository.add("School", "School");
+TextRepository.add("Univesity", "Univesity");
+TextRepository.add("UnivesityAdv", "UnivesityAdv");
+TextRepository.add("RecreationalFacility", "RecreationalFacility");
+TextRepository.add("RecreationalFacilityAdv", "RecreationalFacilityAdv");
+TextRepository.add("Warehouse", "Warehouse");
+TextRepository.add("Smelter", "Smelter");
+TextRepository.add("SmelterAdv", "SmelterAdv");
+TextRepository.add("GoodsFactory", "GoodsFactory");
+TextRepository.add("IndustrialFactory", "IndustrialFactory");
+TextRepository.add("ChemicalFactory", "ChemicalFactory");
+TextRepository.add("PharmaceuticalIndustry", "PharmaceuticalIndustry");
+TextRepository.add("HotLab", "HotLab");
+TextRepository.add("HotLabAdv", "HotLabAdv");
+TextRepository.add("Lab", "Lab");
+TextRepository.add("LabAdv", "LabAdv");
+TextRepository.add("DeepLab", "DeepLab");
+
+TextRepository.add("MineMetal", "MineMetal");
+TextRepository.add("MineCristal", "MineCristal");
+TextRepository.add("MineRadioactive", "MineRadioactive");
+
+TextRepository.add("Park", "Park");
+
+// --- Ricerche e Ricette
+TextRepository.add("ElectrostaticEnergy", "ElectrostaticEnergy");
+TextRepository.add("ElectrostaticEnergy" + "Description", "ElectrostaticEnergy" + "Description");
+
+TextRepository.add("GeothermalEnergy", "GeothermalEnergy");
+TextRepository.add("GeothermalEnergy" + "Description", "GeothermalEnergy" + "Description");
+
+TextRepository.add("Mineralogy", "Mineralogy");
+TextRepository.add("Mineralogy" + "Description", "Mineralogy" + "Description");
+
+TextRepository.add("Metallurgy", "Metallurgy");
+TextRepository.add("Metallurgy" + "Description", "Metallurgy" + "Description");
+
+TextRepository.add("NuclearChemistry", "NuclearChemistry");
+TextRepository.add("NuclearChemistry" + "Description", "NuclearChemistry" + "Description");
+
+TextRepository.add("NuclearPhysics", "NuclearPhysics");
+TextRepository.add("NuclearPhysics" + "Description", "NuclearPhysics" + "Description");
+
+TextRepository.add("ParticlePhysics", "ParticlePhysics");
+TextRepository.add("ParticlePhysics" + "Description", "ParticlePhysics" + "Description");
+
+TextRepository.add("Hydroponics", "Hydroponics");
+TextRepository.add("Hydroponics" + "Description", "Hydroponics" + "Description");
+
+TextRepository.add("Aquaponics", "Aquaponics");
+TextRepository.add("Aquaponics" + "Description", "Aquaponics" + "Description");
+
+TextRepository.add("Medicine", "Medicine");
+TextRepository.add("Medicine" + "Description", "Medicine" + "Description");
+
+TextRepository.add("AdvancedMedicine", "AdvancedMedicine");
+TextRepository.add("AdvancedMedicine" + "Description", "AdvancedMedicine" + "Description");
+
+TextRepository.add("Pharmacology", "Pharmacology");
+TextRepository.add("Pharmacology" + "Description", "Pharmacology" + "Description");
+
+TextRepository.add("Embryology", "Embryology");
+TextRepository.add("Embryology" + "Description", "Embryology" + "Description");
+
+TextRepository.add("Robo-Worker", "Robo-Worker");
+TextRepository.add("Robo-Worker" + "Description", "Robo-Worker" + "Description");
+
+TextRepository.add("Robo-Digger", "Robo-Digger");
+TextRepository.add("Robo-Digger" + "Description", "Robo-Digger" + "Description");
+
+TextRepository.add("Robo-Dozer", "Robo-Dozer");
+TextRepository.add("Robo-Dozer" + "Description", "Robo-Dozer" + "Description");
